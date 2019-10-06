3840c66181b3903853a3933dca2f2bac83741d08
Mark Todd <marktodd.hl@gmail.com>
2019-10-06 16:14:42 +0200

    Add svg loader config

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
