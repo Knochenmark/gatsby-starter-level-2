c3544831db3721bcf4e0f25ab68cd5100b8aa2dd
Mark Todd <marktodd.hl@gmail.com>
2019-11-02 14:48:27 +0100

    Remove antd styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Image from '../components/image';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-import 'antd/dist/antd.css';
 
 const IndexPage = () => (
   <Layout>
