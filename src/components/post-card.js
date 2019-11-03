91147c19ded3e15b6554c9dba63220d988db6ece
Mark Todd <marktodd.hl@gmail.com>
2019-11-03 21:22:23 +0100

    Increase post card click area

--- src/components/post-card.js
+++ src/components/post-card.js
@@ -1,29 +1,24 @@
 import { Link } from 'gatsby';
-import PropTypes from 'prop-types';
 import Img from 'gatsby-image';
+import PropTypes from 'prop-types';
 import React from 'react';
 import style from './post-card.module.styl';
 import PostTag from './post-tag';
 
 const PostCard = ({ title, description, link, coverImage, tags }) => {
-
   const tagsList = tags.map(tag => <PostTag key={tag} tag={tag} />);
 
   return (
-    <div className={`${style.postCard} content-box`}>
-      {coverImage && (
-        <Link to={link}>
-          <Img fluid={coverImage} />
-        </Link>
-      )}
-      <div>
-        <Link className={style.postCardTitleLink} to={link}>
+    <article className={`${style.postCard} content-box`}>
+      <Link to={link}>
+        <div className={style.postCardContent}>
+          {coverImage && <Img fluid={coverImage} />}
           <h2 className="post-card__title">{title}</h2>
-        </Link>
-        <p className="post-card__description">{description}</p>
-        <div className={style.postTags}>{tagsList}</div>
-      </div>
-    </div>
+          <p className="post-card__description">{description}</p>
+        </div>
+      </Link>
+      <div className={style.postTags}>{tagsList}</div>
+    </article>
   );
 };
 
@@ -32,7 +27,7 @@ const coverImageShape = PropTypes.shape({
   base64: PropTypes.string,
   sizes: PropTypes.string,
   src: PropTypes.string,
-  srcSet: PropTypes.string
+  srcSet: PropTypes.string,
 });
 
 PostCard.propTypes = {
@@ -40,7 +35,7 @@ PostCard.propTypes = {
   description: PropTypes.string.isRequired,
   link: PropTypes.string.isRequired,
   tags: PropTypes.arrayOf(PropTypes.string),
-  title: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
 };
 
 export default PostCard;
