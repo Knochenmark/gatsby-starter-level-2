5fd99404c70b31213b15d0917cfb0081afe9e1ca
Mark Todd <mark.todd@westernacher.com>
2019-12-27 12:27:56 +0100

    Add reusable media queries

---
+++ src/components/_shared/media.js
@@ -0,0 +1,19 @@
+export const mq = {
+  xs: '@media screen and (max-width: 599px)',
+  sm: '@media screen and (min-width: 600px) and (max-width: 959px)',
+  md: '@media screen and (min-width: 960px) and (max-width: 1279px)',
+  lg: '@media screen and (min-width: 1280px) and (max-width: 1919px)',
+  xl: '@media screen and (min-width: 1920px) and (max-width: 5000px)',
+  lt: {
+    sm: '@media screen and (max-width: 599px)',
+    md: '@media screen and (max-width: 959px)',
+    lg: '@media screen and (max-width: 1279px)',
+    xl: '@media screen and (max-width: 1919px)',
+  },
+  gt: {
+    xs: '@media screen and (min-width: 600px)',
+    sm: '@media screen and (min-width: 960px)',
+    md: '@media screen and (min-width: 1280px)',
+    lg: '@media screen and (min-width: 1920px)',
+  },
+};
