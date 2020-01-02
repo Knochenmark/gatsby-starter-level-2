a1c60b0e9e2bb7859bc21b6a3031db9bd06bb409
Mark Todd <mark.todd@westernacher.com>
2020-01-02 08:09:06 +0100

    Migrate post card styles to styled components

--- src/components/_shared/styled-mixins.js
+++ src/components/_shared/styled-mixins.js
@@ -5,3 +5,14 @@ export const flexCenter = css`
   justify-content: center;
   align-items: center;
 `;
+
+export const contentBox = css`
+  background-color: var(--bg-content-color);
+  max-width: var(--content-width);
+  margin: 0 auto;
+  transition: background-color 0.6s;
+  padding: var(--space);
+  border-radius: var(--radius);
+  box-shadow: 1px 1px 5px 0 rgba(0, 0, 0, 0.02), 1px 1px 15px 0 rgba(0, 0, 0, 0.03);
+  transition: transform 0.3s, background-color 0.3s, box-shadow 0.6s;
+`;
