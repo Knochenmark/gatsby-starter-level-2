c5b44dc92e324acac8cee90acb595a0f0109029e
Mark Todd <marktodd.hl@gmail.com>
2020-03-12 13:03:34 +0100

    Add scroll to top button

---
+++ src/components/scroll-to-top.js
@@ -0,0 +1,39 @@
+import styled from '@emotion/styled';
+import React from 'react';
+import ScrollUpButton from 'react-scroll-up-button';
+import Arrow from '../assets/arrow.svg';
+import { flexCenter } from './_shared/styled-mixins';
+
+const StyledContainer = styled.div`
+  ${flexCenter};
+  padding: 0.5rem;
+  cursor: pointer;
+
+  & svg {
+    transform: rotate(-90deg);
+    fill: var(--title-color);
+  }
+
+  &:hover svg {
+    fill: var(--link-color);
+  }
+`;
+
+const ScrollToTop = () => {
+  return (
+    <ScrollUpButton
+      ContainerClassName="scroll-top-button-container"
+      TransitionClassName="scroll-top-button-transition"
+      ShowAtPosition={500}
+      AnimationDuration={200}
+    >
+      <StyledContainer title="Scroll to top">
+        <Arrow />
+      </StyledContainer>
+    </ScrollUpButton>
+  );
+};
+
+ScrollToTop.propTypes = {};
+
+export default ScrollToTop;
