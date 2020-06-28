8da1eb69fb67aee25a1bfb6955dda231f8b8ea9b
Mark Todd <marktodd.hl@gmail.com>
2020-06-28 13:38:54 +0200

    Add language attribute to html

--- src/components/seo.js
+++ src/components/seo.js
@@ -23,24 +23,18 @@ const SEO = ({ title, description, image, article }) => {
       <meta name="image" content={seo.image} />
 
       {seo.url && <meta property="og:url" content={seo.url} />}
-
       {(article ? true : null) && <meta property="og:type" content="article" />}
-
       {seo.title && <meta property="og:title" content={seo.title} />}
-
       {seo.description && <meta property="og:description" content={seo.description} />}
-
       {seo.image && <meta property="og:image" content={seo.image} />}
 
-      <meta name="twitter:card" content="summary_large_image" />
-
+      {/* <meta name="twitter:card" content="summary_large_image" /> */}
       {/* {twitterUsername && <meta name="twitter:creator" content={twitterUsername} />} */}
+      {/* {seo.title && <meta name="twitter:title" content={seo.title} />} */}
+      {/* {seo.description && <meta name="twitter:description" content={seo.description} />} */}
+      {/* {seo.image && <meta name="twitter:image" content={seo.image} />} */}
 
-      {seo.title && <meta name="twitter:title" content={seo.title} />}
-
-      {seo.description && <meta name="twitter:description" content={seo.description} />}
-
-      {seo.image && <meta name="twitter:image" content={seo.image} />}
+      <html lang="en" />
     </Helmet>
   );
 };
